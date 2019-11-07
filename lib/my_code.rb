543e919a329349d3daebf6468f9a7a4c2c7604c1
Karl Matthes <karlmatthes@gmail.com>
2019-11-07 23:48:06 +0000

    Done.

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,23 @@
-# Your Code Here
+def map(source_array)
+  new_array = []
+  source_array.length.times do |index|
+    new_array.push(yield(source_array[index]))
+  end
+  return new_array
+end
+
+def reduce(source_array, starting_point = 0)
+  output = starting_point
+  source_array.length.times do |index|
+    if source_array[index] == false
+      output = true
+    end
+  end
+  
+  source_array.length.times do |index|
+    output = yield(output, source_array[index])
+  end
+  return output
+end
+
+puts 1 && false
