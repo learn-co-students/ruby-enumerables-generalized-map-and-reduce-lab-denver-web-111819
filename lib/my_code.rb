8f4c43c5c7ef2d0422073cc02598d2c09b8df724
Kelly Rudnicki <kelly.rudnicki@comcast.net>
2019-11-12 23:01:55 +0000

    Done.

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,25 @@
-# Your Code Here
+def map(array)
+  new_array = []
+  index = 0
+  while index < array.length 
+    new_array.push(yield(array[index]))
+    index += 1 
+  end 
+  new_array
+end
+
+def reduce(array, starting_value=nil)
+  if starting_value
+    first = starting_value
+    index = 0 
+  else 
+    first = array[0]
+    index = 1
+  end 
+  
+  while index < array.length
+    first = yield(first, array[index])
+    index += 1
+  end
+  first
+end 
